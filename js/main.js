0947a6d7f6b9268c1adaf0713d4717e70624e4b2
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-03 15:26:13 +0100

    prova creazione 16 num casuali

--- js/main.js
+++ js/main.js
@@ -18,12 +18,33 @@ function createMyElement(htmlElement, className) {
 }
 
 
-// // FUNZIONE B - PROVA CREAZIONE FUNZIONE AGGIUNGI CLASSE - DA VERIFICARE
-// // Funzione per aggiungere una classe
-// function addClassHtml(oldClass, newClass) {
-//     const element = document.querySelector(oldClass).classList.add(newClass);
-//     return element;
-// }
+// FUNZIONE B
+// Generazione di un numero casuale da 1 a 100
+function randomNumber() {
+
+    let element = Math.floor(Math.random() * 100) + 1;
+    return element;
+    
+}
+
+// FUNZIONE C - PROVA DA VERIFICARE
+// Controllare se un numero è già incluso o meno in un array
+function isIncluded(arrayNumbers) {
+    const element = [cosametto]
+    if(element.indexOf(arrayNumbers) > -1) {
+        return true;
+    } 
+        return false;
+    
+}
+
+
+//  FUNZIONE D - DA VERIFICARE
+// Funzione per aggiungere una classe
+function addClassHtml(oldClass, newClass) {
+    const element = document.querySelector(oldClass).classList.add(newClass);
+    return element;
+}
 
 
 /* 
@@ -32,34 +53,27 @@ ALTRE ISTRUZIONI
 ************************* 
 */
 
+/* Creo un array di cpuNumbers vuoto e lo popolo con 16 numeri casuali
+grazie ad un ciclo while.*/
+let cpuNumbers = [];
+let createdNum = 0;
+let i = 0;
+while (i < 16) {
+    createdNum = randomNumber();
+    cpuNumbers.push(createdNum);
+    console.log(createdNum);
+    i++
+}
+
+
 
 // Creo una variabile che richiama il contenitore delle celle
 const cellContainer = document.querySelector(".cell_container");
 
-/* Creo una nuova variabile cellBox che riguarda l'elemento creato, 
-cui associo la "funzione A" (vedi FUNZIONI) per creare un div che
-richiama la classe della cella; appendo la nuova variabile al 
-cellContainer; il tutto lo inserisco in un ciclo FOR per replicare
-100 volte l'operazione e creare la griglia di partenza; in più chiedo
-di stampare a console il numero della cella quando la clicco*/
-for (let i = 1; i <= 100; i++) {
-    let cellBox = createMyElement('div', 'cell_box');
-    cellBox.innerText = [i];
-    cellContainer.append(cellBox);
-    cellBox.addEventListener('click', function() {
-       console.log(i);
-}
-
-)
-     
-}
-
-
 // Creo una variabile che richiama il contenitore del bottone
 const buttonContainer = document.querySelector(".btn_box");
 
-
-/* Creo una variabile e aggancio "la funzioen A" per creare il tasto
+/* Creo una variabile e aggancio "la funzione A" per creare il tasto
 bottone e aggiungere la relativa classe; aggiungo la scritta PLAY 
 all'elemento e lo appendo al contenitore button box.*/
 const playButton = createMyElement('button', 'play_button');
@@ -70,16 +84,42 @@ buttonContainer.append(playButton);
 /* Creo una variabile showContainer che si aggancia alla classe
 main_container che contiene l'intera griglia; la griglia è nascosta
 di default al caricamento del sito e si attiva con il click
-sul tasto PLAY; viene aggiunta la classe "show_main_container"*/
+sul tasto PLAY; viene aggiunta la classe "show_main_container".
+All'evento del click inserisco il ciclo FOR per la creazione delle
+100 celle, e successivamente viene mostrato il contenitore delle celle*/
 let showContainer = document.querySelector('.main_container');
 playButton.addEventListener('click',
     
     function() {
-        showContainer.classList.add('show_main_container');
+        
+    for (let i = 1; i <= 100; i++) {
+    let cellBox = createMyElement('div', 'cell_box');
+    cellBox.innerText = [i];
+    cellContainer.append(cellBox);
+    cellBox.addEventListener('click', function() {
+       console.log(i); 
+
+}
+
+)
+     
+}
+
+    showContainer.classList.add('show_main_container');
       
 }
 
 )
+
+
+
+
+
+
+
+
+
+
 
 
 
