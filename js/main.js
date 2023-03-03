ba95cc1531a8f75b53f92837838ff4fa4c939cfa
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-03 17:00:48 +0100

    aggiunta colore casella corretta/errata

--- js/main.js
+++ js/main.js
@@ -23,29 +23,8 @@ function createMyElement(htmlElement, className) {
 function randomNumber() {
 
     let element = Math.floor(Math.random() * 100) + 1;
-    return element;
-    
+    return element;  
 }
-
-// FUNZIONE C - PROVA DA VERIFICARE
-// Controllare se un numero è già incluso o meno in un array
-function isIncluded(arrayNumbers) {
-    const element = [cosametto]
-    if(element.indexOf(arrayNumbers) > -1) {
-        return true;
-    } 
-        return false;
-    
-}
-
-
-//  FUNZIONE D - PROVA DA VERIFICARE
-// Funzione per aggiungere una classe
-function addClassHtml(oldClass, newClass) {
-    const element = document.querySelector(oldClass).classList.add(newClass);
-    return element;
-}
-
 
 /* 
 *************************
@@ -53,19 +32,9 @@ ALTRE ISTRUZIONI
 ************************* 
 */
 
-/* DA VERIFICARE - Creo un array di cpuNumbers vuoto e lo popolo con 16 numeri casuali
-grazie ad un ciclo while.
+/* Creo una variabile di tipo Array vuota che verrà riempita con i 
+valori dei numeri random creati dalla cpu (che rappresentano le bombe)*/
 let cpuNumbers = [];
-let createdNum = 0;
-let i = 0;
-while (i < 16) {
-    createdNum = randomNumber();
-    cpuNumbers.push(createdNum);
-    console.log(createdNum);
-    i++
-}*/
-
-
 
 // Creo una variabile che richiama il contenitore delle celle
 const cellContainer = document.querySelector(".cell_container");
@@ -73,43 +42,46 @@ const cellContainer = document.querySelector(".cell_container");
 // Creo una variabile che richiama il contenitore del bottone
 const buttonContainer = document.querySelector(".btn_box");
 
-/* Creo una variabile e aggancio "la funzione A" per creare il tasto
-bottone e aggiungere la relativa classe; aggiungo la scritta PLAY 
-all'elemento e lo appendo al contenitore button box.*/
+/* Creo una variabile per il bottone PLAY e aggancio "la funzione A" 
+per creare il tasto bottone e aggiungere la relativa classe; 
+aggiungo la scritta PLAY  all'elemento e lo appendo al contenitore button box.*/
 const playButton = createMyElement('button', 'play_button');
 playButton.innerText = 'PLAY';
 buttonContainer.append(playButton);
 
-
-/* Creo una variabile showContainer che si aggancia alla classe
-main_container che contiene l'intera griglia; la griglia è nascosta
-di default al caricamento del sito e si attiva con il click
-sul tasto PLAY; viene aggiunta la classe "show_main_container".
-All'evento del click inserisco il ciclo FOR per la creazione delle
-100 celle, e successivamente viene mostrato il contenitore delle celle*/
+/*  All'evento del click sul bottone play viene mostrato il container contenente 
+la grigia e avvio il ciclo FOR per la creazione delle 100 celle, del numero della 
+cella, e l'impostazione sul click sulla cella che fa partire il gioco; in caso di 
+presenza del numero scelto nell'array contenente le bombe, il giocatore perderà; */
 let showContainer = document.querySelector('.main_container');
-playButton.addEventListener('click',
-    
-    function() {
-        
+playButton.addEventListener('click', function() {   
     for (let i = 1; i <= 100; i++) {
-    let cellBox = createMyElement('div', 'cell_box');
-    cellBox.innerText = [i];
-    cellContainer.append(cellBox);
-    cellBox.addEventListener('click', function() {
-       console.log(i); 
+        let cellBox = createMyElement('div', 'cell_box');
+        cellBox.innerText = [i];
+        cellContainer.append(cellBox);
+        cellBox.addEventListener('click', function() {
+            console.log(i);
+            if (cpuNumbers.includes(i)){
+                cellBox.classList.add('bomb_cell');
 
-}
+            } else {
+                cellBox.classList.add('correct_cell');
+            }
+        })   
+    }
 
-)
-     
-}
+    let createdNum = 0;
+    let i = 0;
+    while (i < 16) {
+        createdNum = randomNumber();
+        cpuNumbers.push(createdNum);
+        console.log(createdNum);
+        i++
+    }
 
     showContainer.classList.add('show_main_container');
       
-}
-
-)
+})
 
 
 
