ebe1c04ca1e759d04f72def50ae6e37a15f63824
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-04 03:30:51 +0100

    sistemazione commenti

--- js/main.js
+++ js/main.js
@@ -6,32 +6,32 @@ FUNZIONI
 ************************* 
 */
 
-// FUNZIONE A
-// Funzione per creare un elemento HTML e agganciarci una classe css
+// FUNZIONE A - creare un elemento HTML e agganciarci una classe css
 function createMyElement(htmlElement, className) {
     const element = document.createElement(htmlElement);  
     element.classList.add(className);
-    
-    return element; 
-    /* Mi creo questa funzione per creare l'elemento
-     <div class="cell_box"></div> */
+    return element; /* Mi creo questa funzione per creare l'elemento <div class="cell_box"></div> */
 }
 
 
-// FUNZIONE B
-// Generazione di un numero casuale da 1 a 100
+// FUNZIONE B - Generazione di un numero casuale da 1 a 100
 function randomNumber() {
 
     let element = Math.floor(Math.random() * 100) + 1;
     return element;  
 }
 
-// FUNZIONE C
-// Resettare/svuotare un elemento html
+
+// FUNZIONE C - Resettare/svuotare un elemento html
 function emptyHtml(htmlElement) {
     htmlElement.innerHTML = '';
 }
 
+
+// FUNZIONE D - Aggiungere una classe da html
+function addClass(element, newClass) {
+    element.classList.add(newClass);
+}
 
 
 /* 
@@ -50,7 +50,7 @@ const cellContainer = document.querySelector(".cell_container");
 // Creo una variabile che richiama il contenitore del bottone
 const buttonContainer = document.querySelector(".btn_box");
 
-/* Creo una variabile per il bottone PLAY e aggancio "la funzione A" 
+/* Creo una variabile per il bottone PLAY e aggancio "la FUNZIONE A" 
 per creare il tasto bottone e aggiungere la relativa classe; 
 aggiungo la scritta PLAY  all'elemento e lo appendo al contenitore button box.*/
 const playButton = createMyElement('button', 'play_button');
@@ -64,7 +64,7 @@ presenza del numero scelto nell'array contenente le bombe, il giocatore perderà
 let showContainer = document.querySelector('.main_container');
 playButton.addEventListener('click', function() { 
     
-    emptyHtml(cellContainer);  //svuota il contenitore celle (Function C); le celle vengono appese col ciclo For di volta in volta
+    emptyHtml(cellContainer);  //svuota il contenitore celle (FUNZIONE C); le celle vengono appese col ciclo For di volta in volta
     
     for (let i = 1; i <= 100; i++) {
         let cellBox = createMyElement('div', 'cell_box');
@@ -72,12 +72,12 @@ playButton.addEventListener('click', function() {
         cellContainer.append(cellBox);
         cellBox.addEventListener('click', function() {
             console.log('numero cliccato: ', i);
-            if (cpuNumbers.includes(i)){ /* controllo se numero cliccato è nell'array*/
-                cellBox.classList.add('bomb_cell');
+            if (cpuNumbers.includes(i)) {      // controllo se numero cliccato è nell'array
+                addClass(cellBox, 'bomb_cell');   // Utilizzo la FUNZIONE D per aggiungere la classe css
                 alert('hai perso, clicca nuovamente sul tasto PLAY');
 
             } else {
-                cellBox.classList.add('correct_cell');
+                addClass(cellBox, 'correct_cell');  // Utilizzo la FUNZIONE D per aggiungere la classe css
                 // contatore numero da inserire;
             }
         })   
